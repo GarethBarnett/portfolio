dbfa0886da5340c4d14b44052eb6bd20ac7eb1d5
GarethBarnett <osd@hotmail.co.nz>
2019-05-30 20:42:32 +1200

    hero, footer construction

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,28 @@
+// let loader = document.getElementById("splash");
+
+// /* Splash Animation */
+// setTimeout(function () {
+//    'use strict';
+//    loader.classList.add('slideOutUp');
+// }, 2000);
+
+
+// /* Splash Removal */
+// setTimeout(function () {
+//    'use strict';
+//    loader.parentNode.removeChild(loader);
+// }, 3000);
+
+
+$("body").css('overflow', 'hidden');
+$(".loader").fadeIn();
+setTimeout(() => {
+$("body").css('overflow', 'unset');
+$(".loader").fadeOut(300);
+}, 2200);
+
+
+
 function resizeHeaderOnScroll() {
     const distanceY = window.pageYOffset || document.documentElement.scrollTop,
     shrinkOn = 200,
@@ -11,3 +36,55 @@ function resizeHeaderOnScroll() {
   }
   
   window.addEventListener('scroll', resizeHeaderOnScroll);
+
+
+
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  speed: 800,
+  // slidesPerView: 3,
+  // spaceBetween: 70,
+  watchSlidesVisibility: !0,
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+
+
+  // breakpoints: {
+  //           768: {
+  //               spaceBetween: 20,
+  //               slidesPerView: 1
+  //           },
+  //           1200: {
+  //               spaceBetween: 30,
+  //               slidesPerView: 2
+  //           },
+  //           1440: {
+  //               spaceBetween: 30,
+  //               slidesPerView: 3
+  //           }
+  //       }
+
+
+
+
+
+  
+});
+
